0537bb6c45c261669e1c8f27693468b3c7eef9ae
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-02-13 19:30:21 +0900

    Created Customer.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+// const customer = {
+//   'id' : 1,
+//   'image' : 'https://placeimg.com/64/64/any',
+//   'name' : '김중원',
+//   'birthday' : '961222',
+//   'gender' : '남자',
+//   'job' : '대학생'
+// }
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/100/100/1',
+    'name' : '김중원',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/100/100/2',
+    'name' : '하지원',
+    'birthday' : '700214',
+    'gender' : '여자',
+    'job' : '가수'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/100/100/3',
+    'name' : '김가연',
+    'birthday' : '901122',
+    'gender' : '여자',
+    'job' : '보안관'
+  }
+]
 
 class App extends Component {
   render() {
@@ -8,11 +45,29 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" lat="logo"alt="logo" />
-          <div className="gray-background">
-            <h1>Hello React Project</h1>
-            <h2>Let's develope management system</h2>
-          </div>
+          <Customer
+            id={customers[0].id}
+            image={customers[0].image}
+            name={customers[0].name}
+            birthday={customers[0].birthday}
+            gender={customers[0].gender}
+            job={customers[0].job}
+          />
+          {
+            customers.map(c => {
+              return <Customer key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            })
+          }
         </header>
+
+        
       </div>
     )
   }
---
+++ src/components/Customer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        
+        return (
+            <div>
+                <CustomerProfile
+                    id={this.props.id}
+                    image={this.props.image}
+                    name={this.props.name}
+                />
+                <CustomerInfo
+                    birthday={this.props.birthday}
+                    gender={this.props.gender}
+                    job={this.props.job}
+                />
+            </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name} ( {this.props.id} )</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+
+//다른 컴포넌트에서 사용가능하게 함
+export default Customer;
