2f4b46190554de2515d78a6eab5445bba578e204
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-02-18 19:50:49 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
+
 
 const styles = theme => ({
   root: {
@@ -19,17 +21,36 @@ const styles = theme => ({
   },
   table: {
     minWidth : 1080
+  },
+  progress : {
+    //margin : theme.spacing.unit * 2
+    margin : theme.spacing(3)
   }
 });
 
+/*
+React LifeCycle
+처음 컴포넌트를 구성할때
+1) constructor()
+2) componetWillMount
+3) render()
+4) componentDidMount()
+*/
+/*
+컴포넌트의 props나 state가 변경될때
+shouldComponetUpdate() -> render()
+*/
 
 class App extends Component {
 
   state = {
-    customers: "" //변경될수있는데이터를처리하려고 할때
+    customers : "", //변경될수있는데이터를처리하려고 할때
+    completed : 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20); //0.02마다 progress 함수 실행
+    
     this.callApi()
       .then(res => this.setState({customers : res}))
       .catch(err => console.log(err));
@@ -39,6 +60,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed : completed >= 100 ? 0 : completed+1 })
   }
 
   render() {
@@ -65,9 +91,14 @@ class App extends Component {
                   name={c.name}
                   birthday={c.birthday}
                   gender={c.gender}
-                  job={c.job}
-                />
-              }) : ""
+                  job={c.job} />
+              }) :
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" 
+                                    value={this.state.completed} />
+                </TableCell>
+              </TableRow>
             }
           </TableBody>
         </Table>
