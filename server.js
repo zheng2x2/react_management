85e118fde3cf7d9238e2af68b6b4ba1e6f30763e
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-02-19 23:53:49 +0900

    Add Node.js db connection setting to MySql on AWS & Crate table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,37 +7,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+conn.connect();
+
 app.get('/api/hello', (req, res) => {
     res.send({ message : 'Hello Express '});
 });
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'https://placeimg.com/100/100/1',
-            'name' : '김중원',
-            'birthday' : '961222',
-            'gender' : '남자',
-            'job' : '대학생'
-          },
-          {
-            'id' : 2,
-            'image' : 'https://placeimg.com/100/100/2',
-            'name' : '하지원',
-            'birthday' : '700214',
-            'gender' : '여자',
-            'job' : '가수'
-          },
-          {
-            'id' : 3,
-            'image' : 'https://placeimg.com/100/100/3',
-            'name' : '김가연',
-            'birthday' : '901122',
-            'gender' : '여자',
-            'job' : '보안관'
-          }
-    ]);
+    conn.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
